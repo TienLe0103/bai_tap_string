024e5df53bbccd7bf021a36822e155fd3b1a04d4
Le Huynh Tien <107425806+TienLe0103@users.noreply.github.com>
2023-09-19 15:52:27 +0700

    Bai_5.cpp

--- Bai_5.cpp
+++ Bai_5.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <string>
-#include <cctype>
 
 bool MK_AN_TOAN(const std::string& s) {
     bool IN_HOA = false;
     bool IN_THUONG = false;
     bool CHU_SO = false;
     for (char c : s) {
-        if (std::isupper(c)) //ktra ky tu in hoa
+        if ((int)c >= 65 && (int)c <= 90) {
             IN_HOA = true;
-        else if (std::islower(c)) //ktra ky tu in thuong
+        } else if ((int)c >= 97 && (int)c <= 122) { 
             IN_THUONG = true;
-        else if (std::isdigit(c)) //ktra ky tu so
+        } else if ((int)c >= 48 && (int)c <= 57) { 
             CHU_SO = true;
+        }
+        if (IN_HOA && IN_THUONG && CHU_SO) {
+            break;
+        }
     }
     return IN_HOA && IN_THUONG && CHU_SO && s.length() >= 6;
 }
 
 int main() {
+    std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
     std::string password;
     std::cin >> password;
     int count = 0;
